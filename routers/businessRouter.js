94a1623f1b5ab6930437952e5876fe277041ae94
srini_11 <srisrinivasan251@gmail.com>
2025-03-12 18:23:02 +0530

    Router updated to process the request

--- routers/businessRouter.js
+++ routers/businessRouter.js
@@ -1,13 +1,14 @@
 // Required modules
 import express from "express";
+import { processesRequest } from "../controllers/controller";
 
 const router = express.Router();
 
-router.post('/getProducts');
-router.post('/addProduct');
-router.post('/editProduct');
-router.post('/deleteProducts');
-router.post('/getProduct');
+router.post('/getProducts', processesRequest);
+router.post('/addProduct', processesRequest);
+router.post('/editProduct', processesRequest);
+router.post('/deleteProducts', processesRequest);
+router.post('/getProduct', processesRequest);
 
 // Export
 export { router as businessRouter };
